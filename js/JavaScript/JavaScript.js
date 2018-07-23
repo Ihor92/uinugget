dab759f5d9a9b2d20b0db97990636cc1e1f59ac4
Ihor92 <salfetnikov92@gmail.com>
2018-07-23 17:43:25 +0300

    New project

---
+++ js/JavaScript/JavaScript.js
@@ -0,0 +1,10 @@
+// document.write ("Hello world!");
+// document.write ("<br /> Hello world!");
+// alert('Hello world');
+// alert('godbay world');
+'use strict';
+var years = promt('Сколько вам лет?', 100);
+alert('Вам ' + years + ' лет!');
+		  
+var isAdmin = confirm("Вы - администратор?");
+alert( isAdmin );
